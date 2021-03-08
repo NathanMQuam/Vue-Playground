936366c51587d0f6c7041af71d4e75d713be43c1
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-03-08 13:48:01 -0700

    Expressions Exercise

--- src/components/ExpressionsExercise.vue
+++ src/components/ExpressionsExercise.vue
@@ -9,16 +9,27 @@
             below:
           </p>
 
-          <p>{{}}</p>
+          <p>{{2+2 * 2 /2}}</p>
         </div>
         <div class="border p-1">
           <p>
             Use interpolation to get the customary Hello, World! to display on
             the screen. Don't forget your quotes
           </p>
-          <p>{{}}</p>
+          <p>{{phrase}}</p>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+  import { reactive, toRefs } from "vue";
+  export default {
+    setup() {
+      const output = reactive({phrase: 'Hello World!'})
+
+      return toRefs(output)
+    }
+  }
+</script>
